5aba666fc2ac064a6d9b6169b1cb95c651626c2f
AlexandreLadeira <32870665+AlexandreLadeira@users.noreply.github.com>
2018-07-03 14:48:20 -0300

    Mudanças de férias
    Adicionei dois arquivos sobre feiras: Um sobre o roteiro do video apra a Bragantec e outro sobre a nossa inscricao.
    Tambem comecei a implementacao do alto contraste do site, só falta definir as cores certinho, mas quando clica ja muda o estilo :D

--- Site/js/app.js
+++ Site/js/app.js
@@ -1,1 +1,18 @@
-$(document).foundation()
+function trocaEstilo(arq) {
+
+	if(document.getElementById('modo').innerHTML == "")
+	{
+		document.getElementById('estilo').setAttribute('href','css/altoContraste.css');
+		document.getElementById('modo').innerHTML = "altoContraste";
+	}
+	else
+	{
+		document.getElementById('estilo').setAttribute('href','css/app.css');
+		document.getElementById('modo').innerHTML = "";
+	}
+
+	
+	
+
+	console.log(arq);
+}
