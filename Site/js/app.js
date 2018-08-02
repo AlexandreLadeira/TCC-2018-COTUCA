5705ff7eb1961b883cf3d551cdf14a9bf89d717b
AlexandreLadeira <32870665+AlexandreLadeira@users.noreply.github.com>
2018-08-02 11:52:43 -0300

    Mudancas na teoria para suportar o audio
    Trabalhando com o textToSpeech. Ainda não está funcionando, precisamos arruma erros

--- Site/js/app.js
+++ Site/js/app.js
@@ -22,7 +22,7 @@ function moeda(z){
 	v = z;
 	v = v.replace(",",".");	
 	return v;
-	}
+}
 
 function CorridaUber(){
 	var bandeira 	= parseFloat(moeda(document.getElementById("bandeira").value));
@@ -40,5 +40,40 @@ function CorridaUber(){
 	resultado = resultado.replace(".",",");
 
 	document.getElementById("resultado").innerHTML = "R$" + resultado; 
+}
+
+
+function isPlaying(audelem) { return !audelem.paused; }
+
+function falar(texto){       
+	
+	var vet   = texto.split("."); 
+	var i	  = 0;
+	var audio = document.getElementById('som');
+
+	
+	while (i < vet.length)
+	{
+		if(!(isPlaying(audio)))
+		{
+			audio.src = "http://translate.google.com/translate_tts?tl=pt&q="+ vet[i] +"&client=tw-ob";
+			audio.play(); // funciona 
+			i++;
+		}
+	}
+
+	//https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_av_event_ended
+	
+
+
+/*	while (i < vet.length)
+	{
+		
+			var url  = "http://translate.google.com/translate_tts?tl=pt&q="+ vet[i] +"&client=tw-ob";
+			$('audio').attr('src',url).get(0).play();//play no audio 
+			i++;
+		
+	} */
+
 }
 
